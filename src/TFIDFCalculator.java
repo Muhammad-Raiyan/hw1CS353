29f00a23ba679c0b8b19c5ed2ce3b088d6c3d0d1
Muhammad-Raiyan <mislam0525@gmail.com>
2017-03-19 00:16:32 -0400

    Implemented tfidf

---
+++ src/TFIDFCalculator.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ishmam on 3/19/2017.
+ *
+ * @author ishmam
+ */
+public class TFIDFCalculator {
+
+    public double tf(DataModel dm, String term) {
+        double result = 0;
+
+        List<String> file = dm.getContent();
+        for (String word : file) {
+            if (term.equalsIgnoreCase(word))
+                result++;
+        }
+        return result / file.size();
+    }
+
+
+    public double idf(ArrayList<DataModel> docs, String term) {
+
+        double n = 0;
+        for (DataModel dm : docs) {
+            ArrayList<String> content = dm.getContent();
+            for (String word : content) {
+                if (term.equalsIgnoreCase(word)) {
+                    n++;
+                    break;
+                }
+            }
+        }
+        return Math.log(docs.size() / n);
+    }
+
+
+    public double tfIdf(DataModel file, ArrayList<DataModel> docs, String term) {
+        return tf(file, term) * idf(docs, term);
+
+    }
+}
